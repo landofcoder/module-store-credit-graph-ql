1ab60e8dc1657210248d7dfc6f5f88d894f9274e
landofcoder <landofcoder@gmail.com>
2020-11-22 14:11:49 +0700

    updated graphql for store credit

--- Model/Resolver/CustomerCreditTransaction.php
+++ Model/Resolver/CustomerCreditTransaction.php
@@ -26,13 +26,55 @@ namespace Lof\StoreCreditGraphQl\Model\Resolver;
 use Magento\Framework\GraphQl\Config\Element\Field;
 use Magento\Framework\GraphQl\Query\ResolverInterface;
 use Magento\Framework\GraphQl\Schema\Type\ResolveInfo;
-
+use Magento\Catalog\Api\Data\CategoryInterface;
+use Magento\Framework\Exception\InputException;
+use Magento\Framework\GraphQl\Exception\GraphQlInputException;
+use Magento\Framework\GraphQl\Query\Resolver\Argument\SearchCriteria\ArgumentApplier\Filter;
+use Magento\Framework\GraphQl\Query\Resolver\Argument\SearchCriteria\ArgumentApplier\Sort;
+use Magento\Search\Model\Query;
+use Magento\Store\Api\Data\StoreInterface;
+use Magento\Store\Model\ScopeInterface;
 /**
  * Class GiftByProductSku
  * @package Lof\StoreCreditGraphQl\Model\Resolver
  */
-class CustomerCreditTransaction extends AbstractStoreCreditQuery implements ResolverInterface
+class CustomerCreditTransaction implements ResolverInterface
 {
+    /**
+     * @var string
+     */
+    private const SPECIAL_CHARACTERS = '-+~/\\<>\'":*$#@()!,.?`=%&^';
+
+    /**
+     * @var ScopeConfigInterface
+     */
+    private $scopeConfig;
+
+    /**
+     * @var CreditManagementInterface
+     */
+    private $creditManagement;
+
+    /**
+     * @var Builder
+     */
+    private $searchCriteriaBuilder;
+
+    /**
+     * @param ScopeConfigInterface $scopeConfig
+     * @param CreditManagementInterface $creditManagement
+     * @param Builder $searchCriteriaBuilder
+     */
+    public function __construct(
+        ScopeConfigInterface $scopeConfig,
+        CreditManagementInterface $creditManagement,
+        Builder $searchCriteriaBuilder
+    ) {
+        $this->scopeConfig = $scopeConfig;
+        $this->creditManagement = $creditManagement;
+        $this->searchCriteriaBuilder = $searchCriteriaBuilder;
+    }
+
     /**
      * @inheritDoc
      */
@@ -41,8 +83,98 @@ class CustomerCreditTransaction extends AbstractStoreCreditQuery implements Reso
         $customer_id = $context->getUserId();
         if(!$customer_id){
             throw new GraphQlInputException(__('Required logged in customer account.'));
-        }  
-        
-        return $this->_creditManagement->getCreditBalanceByCustId($customer_id, $this->searchCriteriaBuilder);
+        }
+        $store = $context->getExtensionAttributes()->getStore();
+
+        if (isset($args['currentPage']) && $args['currentPage'] < 1) {
+            throw new GraphQlInputException(__('currentPage value must be greater than 0.'));
+        }
+        if (isset($args['pageSize']) && $args['pageSize'] < 1) {
+            throw new GraphQlInputException(__('pageSize value must be greater than 0.'));
+        }
+        if (!isset($args['filters'])) {
+            //When no filters are specified, get the root category
+            $args['filters']['customer_id'] = ['eq' => $customer_id];
+        }
+        try {
+            $filterResult = $this->getTransactionResult($args, $store);
+        } catch (InputException $e) {
+            throw new GraphQlInputException(__($e->getMessage()));
+        }
+        return $filterResult;
+    }
+
+    /**
+     * Search for transactions
+     *
+     * @param array $criteria
+     * @param StoreInterface $store
+     * @return mixed
+     * @throws InputException
+     */
+    public function getTransactionResult(array $criteria, StoreInterface $store)
+    {
+        $criteria[Filter::ARGUMENT_NAME] = $this->formatMatchFilters($criteria['filters'], $store);
+        $criteria[Sort::ARGUMENT_NAME]['transaction_id'] = ['DESC'];
+        $searchCriteria = $this->searchCriteriaBuilder->build('lofCustomerCreditTransaction', $criteria);
+        $pageSize = $criteria['pageSize'] ?? 20;
+        $currentPage = $criteria['currentPage'] ?? 1;
+        $searchCriteria->setPageSize($pageSize)->setCurrentPage($currentPage);
+
+        $transactions = $this->creditManagement->getCreditBalanceByCustId($searchCriteria);
+
+        $totalPages = 0;
+        if ($transactions->getTotalCount() > 0 && $searchCriteria->getPageSize() > 0) {
+            $totalPages = ceil($transactions->getTotalCount() / $searchCriteria->getPageSize());
+        }
+        if ($searchCriteria->getCurrentPage() > $totalPages && $transactions->getTotalCount() > 0) {
+            throw new GraphQlInputException(
+                __(
+                    'currentPage value %1 specified is greater than the %2 page(s) available.',
+                    [$searchCriteria->getCurrentPage(), $totalPages]
+                )
+            );
+        }
+
+        return [
+            'items' => $transactions->getItems(),
+            'total_count' => $transactions->getTotalCount(),
+            'page_info' => [
+                'total_pages' => $totalPages,
+                'page_size' => $searchCriteria->getPageSize(),
+                'current_page' => $searchCriteria->getCurrentPage(),
+            ]
+        ];
+    }
+
+    /**
+     * Format match filters to behave like fuzzy match
+     *
+     * @param array $filters
+     * @param StoreInterface $store
+     * @return array
+     * @throws InputException
+     */
+    private function formatMatchFilters(array $filters, StoreInterface $store): array
+    {
+        $minQueryLength = $this->scopeConfig->getValue(
+            Query::XML_PATH_MIN_QUERY_LENGTH,
+            ScopeInterface::SCOPE_STORE,
+            $store
+        );
+
+        foreach ($filters as $filter => $condition) {
+            $conditionType = current(array_keys($condition));
+            if ($conditionType === 'match') {
+                $searchValue = trim(str_replace(self::SPECIAL_CHARACTERS, '', $condition[$conditionType]));
+                $matchLength = strlen($searchValue);
+                if ($matchLength < $minQueryLength) {
+                    throw new InputException(__('Invalid match filter. Minimum length is %1.', $minQueryLength));
+                }
+                unset($filters[$filter]['match']);
+                $filters[$filter]['like'] = '%' . $searchValue . '%';
+            }
+        }
+        return $filters;
     }
 }
